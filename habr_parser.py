57d451011989c5e927acf8a02c9b5a72aaca6704
MoonlightEnjoyer <artiom.labietskii@gmail.com>
2023-08-01 22:48:21 +0300

    Initial commit

---
+++ habr_parser.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import random
+import json
+import requests
+import datetime
+from fake_useragent import UserAgent
+import re
+
+ua = UserAgent()
+
+headers = {
+    'accept': 'application/json, text/plain, */*',
+    'user-Agent': ua.google,
+}
+
+article_dict = {}
+
+
+url = f'https://freelance.habr.com/tasks/'
+
+res = requests.get(url, headers=headers)
+res.close()
+req = res.text
+
+soup = BeautifulSoup(req, 'lxml')
+categories = soup.find_all('div', class_='task__title')
+
+for category in categories:
+    # print(f'https://companies.rbc.ru{category.get("href")}')
+    print(category.get('title'))
+    #subcategories = soup.find_all('a', class_='block mb-[12px] text-[15px] font-medium leading-[1.6]')
+
