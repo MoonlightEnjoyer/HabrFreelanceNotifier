889d659413ce779dddce851f5987acd1e4c3b780
artiom.labietskii <artiom.labietskii@gmail.com>
2023-08-02 19:31:41 +0300

    Minor

--- habr_parser.py
+++ habr_parser.py
@@ -5,28 +5,29 @@ import requests
 import datetime
 from fake_useragent import UserAgent
 import re
+import telebot
 
-ua = UserAgent()
+# ua = UserAgent()
 
-headers = {
-    'accept': 'application/json, text/plain, */*',
-    'user-Agent': ua.google,
-}
+# headers = {
+#     'accept': 'application/json, text/plain, */*',
+#     'user-Agent': ua.google,
+# }
 
-article_dict = {}
+# article_dict = {}
 
 
-url = f'https://freelance.habr.com/tasks/'
+# url = f'https://freelance.habr.com/tasks/'
 
-res = requests.get(url, headers=headers)
-res.close()
-req = res.text
+# res = requests.get(url, headers=headers)
+# res.close()
+# req = res.text
 
-soup = BeautifulSoup(req, 'lxml')
-categories = soup.find_all('div', class_='task__title')
+# soup = BeautifulSoup(req, 'lxml')
+# categories = soup.find_all('div', class_='task__title')
 
-for category in categories:
-    # print(f'https://companies.rbc.ru{category.get("href")}')
-    print(category.get('title'))
-    #subcategories = soup.find_all('a', class_='block mb-[12px] text-[15px] font-medium leading-[1.6]')
+# for category in categories:
+#     # print(f'https://companies.rbc.ru{category.get("href")}')
+#     print(category.get('title'))
+#     #subcategories = soup.find_all('a', class_='block mb-[12px] text-[15px] font-medium leading-[1.6]')
 
